158d4d6f8d3067eba064dcb841e15c3b2555ab4f
Gaurav Arora <gauravarora.epri@gmail.com>
2018-11-25 23:49:29 +0530

    Update Example1.java and commit to master branch
    added one line to Example1.java

--- Example1.java
+++ Example1.java
@@ -4,7 +4,8 @@ class Example1
 public static void main(String str[])
 {
 System.out.println("First file created and committed to the main branch in GauravWorkSpaceRepository");
-System.out.println("It is created and committed after the Readme.md file which created while creating the repository");  
+System.out.println("It is created and committed after the Readme.md file which created while creating the repository"); 
+System.out.println("Update this file with this line and save/commit to a main branch called master"); 
 }
 
 }
