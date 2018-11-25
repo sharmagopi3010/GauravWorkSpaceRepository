71e9fcb2b84209e340d50ffea8cffb280530c428
Gaurav Arora <gauravarora.epri@gmail.com>
2018-11-25 21:29:15 +0530

    Update Example1.java
    Add one line: System.out.println("It is created and committed after the Readme.md file which created while creating the repository");

--- Example1.java
+++ Example1.java
@@ -1,9 +1,10 @@
-class Example
+class Example1
 {
 
 public static void main(String str[])
 {
 System.out.println("First file created and committed to the main branch in GauravWorkSpaceRepository");
+System.out.println("It is created and committed after the Readme.md file which created while creating the repository");  
 }
 
 }
